d0cffa1338e4f51c9984d9dba9a30098f7be5837
Kevin Marcellier <kevin.marcellier@stack-labs.com>
2021-01-08 12:23:10 +0100

    chore(questions): add some questions

--- question/personal.js
+++ question/personal.js
@@ -3,6 +3,10 @@ var personalQuestions = [
     "Thé ou café ?",
     "Ta boisson préférée ?",
     "Raclette ou Fondue ?",
+    "Kebab ou tacos ?",
+    "Cassoulet ou tajine ?",
+    "Chien ou chat ?",
+    "Croissant, chocolatine ou pain au chocolat ?",
     "Ta couleur préférée ?",
     "Android ou iOS ?",
     "As-tu une phobie ?",
@@ -17,30 +21,36 @@ var personalQuestions = [
     "Où es-tu né(e) ?",
     "Tu habites où ?",
     "Ton dernier repas ?",
+    "Parle nous d'une de tes passions !",
+    "Tu cuisines ou tu te fais livrer ?",
+    "Tu as un animal de compagnie ?",
 
     // Cinéma
     "Quel Disney en #1 ?",
-    "Un résumé en 3 mots de Star Wars !",
+    "Ton animé préféré ?",
     "Si tu ne devais garder que trois films...",
     "Tu regardes une série en ce moment ?",
     "Le dernier film que tu as vu au cinéma ?",
     "Plutôt VF, VO ou VOST ?",
     "Cite nous 3 de tes séries préférées",
+    "Une série à nous conseiller ?",
 
     // Musique
     "La chanson que tu écoutes le plus en ce moment ?",
+    "Ta chanson préferée de ta décénnie",
     "Ton dernier concert ?",
     "Une de tes chansons préférées ?",
     "Ton style de musique ?",
     "Cite 3 de tes artistes musicaux préférés",
 
-    // Sport
+    // Sport & loisir
     "Tu pratiques un sport ?",
     "Tu regardes du sport à la télé ?",
     "Tu as une console de jeux ?",
     "Ton jeu vidéo préféré ?",
+    "Chasse, pêche ou traditions ?",
 
     // Divers
-    "Si tu n'avais pas travailler dans l'IT, tu aurais voulu faire quoi ?"
-
+    "Si tu n'avais pas travailler dans l'IT, tu aurais voulu faire quoi ?",
+    "Quel réseau social tu utilises le plus ?",
 ];
